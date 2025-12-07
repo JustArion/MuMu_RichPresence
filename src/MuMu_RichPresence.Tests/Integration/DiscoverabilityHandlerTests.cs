4c466cc15ea768e65940801f22f9a35057e09333
arion <53127594+JustArion@users.noreply.github.com>
2025-12-08 00:02:11 +0100

    Add a test for the Discoverability Handler

---
+++ src/MuMu_RichPresence.Tests/Integration/DiscoverabilityHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Dawn.MuMu.RichPresence.Discord;
+using FluentAssertions;
+
+namespace MuMu_RichPresence.Tests.Integration;
+
+[TestFixture(TestOf =  typeof(DiscoverabilityHandler))]
+public class DiscoverabilityHandlerTests
+{
+    [TestCase]
+    public async Task Should_ReturnEntries()
+    {
+        // Arrange
+        var handler = new DiscoverabilityHandler();
+
+        // Act
+        var applicationId = await handler.TryGetOfficialApplicationId("Clash of Clans");
+
+        // Assert
+        applicationId
+            .Should()
+            .NotBeNullOrWhiteSpace();
+
+        handler._discoverablePresences
+            .Should()
+            .HaveCountGreaterThan(1000);
+
+        handler._discoverablePresences
+            .Should()
+            .AllSatisfy(x =>
+            {
+                x.Name
+                    .Should()
+                    .NotBeNullOrWhiteSpace();
+                
+                x.Id
+                    .Should()
+                    .NotBeNullOrWhiteSpace();
+            });
+
+    }
+}
