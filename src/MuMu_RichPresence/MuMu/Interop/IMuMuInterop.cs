ff6f34537663050efc5b6da8f691932e7691fbe0
arion <53127594+JustArion@users.noreply.github.com>
2026-01-15 18:31:31 +0100

    Add GetFocusedApp to interface

--- src/MuMu_RichPresence/MuMu/Interop/IMuMuInterop.cs
+++ src/MuMu_RichPresence/MuMu/Interop/IMuMuInterop.cs
@@ -1,7 +1,11 @@
-﻿namespace Dawn.MuMu.RichPresence.MuMu.Interop;
+﻿using Dawn.MuMu.RichPresence.Models;
+
+namespace Dawn.MuMu.RichPresence.MuMu.Interop;
 
 public interface IMuMuInterop
 {
+    public Task<AndroidProcess?> GetFocusedApp(CancellationToken token = default);
+
     public Task<AppInfo> GetForegroundAppInfo(CancellationToken token = default);
 
     public Task<string> GetInfo(AppInfo info, string path, CancellationToken token = default);
