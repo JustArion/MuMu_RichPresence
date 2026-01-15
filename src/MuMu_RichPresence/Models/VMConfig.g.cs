0a0673e807782f09255e1592e1eeb652877abef5
arion <53127594+JustArion@users.noreply.github.com>
2026-01-15 18:21:54 +0100

    Create only-somewhat necessary poco

---
+++ src/MuMu_RichPresence/Models/VMConfig.g.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using System.Text.Json.Serialization;
+
+namespace Dawn.MuMu.RichPresence.MuMu.Interop;
+
+    public record Port(
+        [property: JsonPropertyName("guest_ip")] string? GuestIP,
+        [property: JsonPropertyName("host_port")] string HostPort
+    );
+    public record Nat(
+        [property: JsonPropertyName("port_forward")] PortForward PortForward
+    );
+
+    public record PortForward(
+        [property: JsonPropertyName("adb")] Port ADB,
+        [property: JsonPropertyName("api")] Port API,
+        [property: JsonPropertyName("event")] Port Event,
+        [property: JsonPropertyName("frontend")] Port Frontend,
+        [property: JsonPropertyName("gateway")] Port Gateway,
+        [property: JsonPropertyName("input")] Port Input
+    );
+
+    public record VMConfig(
+        [property: JsonPropertyName("vm")] Vm VM
+    );
+
+    public record Vm(
+        [property: JsonPropertyName("nat")] Nat NAT
+    );
+
+
