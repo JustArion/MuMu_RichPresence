d94c423c7ae2f7bf4d90f54a43b2c269a9864081
arion <53127594+JustArion@users.noreply.github.com>
2026-01-16 03:51:00 +0100

    Add a static stub

--- src/MuMu_RichPresence/MuMu/Interop/AnonymousAsyncDisposable.cs
+++ src/MuMu_RichPresence/MuMu/Interop/AnonymousAsyncDisposable.cs
@@ -1,9 +1,13 @@
 ﻿namespace Dawn.MuMu.RichPresence.MuMu.Interop;
 
-public class AnonymousAsyncDisposable(Func<ValueTask> dispose) : IAsyncDisposable
+public class AnonymousAsyncDisposable(Func<ValueTask>? dispose = null) : IAsyncDisposable
 {
+    public static readonly IAsyncDisposable None = new AnonymousAsyncDisposable();
     public async ValueTask DisposeAsync()
     {
+        if (dispose == null)
+            return;
+
         GC.SuppressFinalize(this);
         await dispose();
     }
