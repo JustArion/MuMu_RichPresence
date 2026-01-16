bf559816900159aebbc726c54cc938c6d3f0709e
arion <53127594+JustArion@users.noreply.github.com>
2026-01-16 03:54:13 +0100

    Don't log executions unless specified (It will fill your log file on debug)

--- src/MuMu_RichPresence/MuMu/Interop/InteropHelper.cs
+++ src/MuMu_RichPresence/MuMu/Interop/InteropHelper.cs
@@ -1,8 +1,11 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿#define LISTEN_TO_INTEROP
+// #define LISTEN_TO_EXECUTIONS
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Reactive.Disposables;
 using System.Text;
 using CliWrap;
+using Dawn.MuMu.RichPresence.Exceptions;
 
 namespace Dawn.MuMu.RichPresence.MuMu.Interop;
 
@@ -13,8 +16,10 @@ public static class InteropHelper
     {
         public async Task<IAsyncDisposable> Connect(CancellationToken token = default)
         {
-            if (!await info.ConnectInternal(token: token) && !await info.ConnectInternal(true, token))
-                throw new Exception("Could not connect to ADB");
+            // We do the fallback first then the specified port
+            // On my observation, the fallback port is more used in my case, this should theoretically shave off +- 5sec to initial ADB connections
+            if (!await info.ConnectInternal(useFallback: true, token) && !await info.ConnectInternal(useFallback: false, token))
+                throw new NotConnectedException("Could not connect to ADB");
 
             return new AnonymousAsyncDisposable(async ()=> await info.Disconnect());
         }
@@ -30,27 +35,39 @@ public static class InteropHelper
             var stdOut = new StringBuilder();
             var stdErr = new StringBuilder();
 
+            var ts = Stopwatch.GetTimestamp();
             var result = await Cli.Wrap(info.ADBPath)
                 .WithValidation(CommandResultValidation.None)
                 .WithArguments(arg)
                 .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOut))
                 .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErr))
                 .ExecuteAsync(token);
+            var elapsed = Stopwatch.GetElapsedTime(ts);
 
-            var @out = stdOut.ToString();
-            var err = stdErr.ToString();
-            Log.Debug("[Exec] adb {Args} -> {Output}{Errors}", arg, @out, err);
+            var @out = stdOut.ToString().Trim();
+            var err = stdErr.ToString().Trim();
+
+            #if LISTEN_TO_INTEROP
+            Log.Debug("[Exec] adb {Args} -> {Output}{StdErr}", arg, @out, err);
+            #endif
 
             const string ERROR = "cannot connect";
             if (@out.Contains(ERROR) || err.Contains(ERROR))
                 return false;
 
-            return result.ExitCode == 0;
+            var success = result.ExitCode == 0;
+
+            if (success)
+                Log.Debug("Connected to MuMuNxDevice in {Connection:F2} sec", elapsed.TotalSeconds);
+
+            return success;
         }
 
         private async Task Disconnect()
         {
+            #if LISTEN_TO_INTEROP
             Log.Debug("[Exec] adb disconnect");
+            #endif
             await Cli.Wrap(info.ADBPath)
                 .WithArguments("disconnect")
                 .ExecuteAsync();
@@ -58,26 +75,33 @@ public static class InteropHelper
 
         public async Task<T> Execute<T>(string command, CancellationToken token = default) where T : IParsable<T>
         {
+            var arg = $"shell {command}";
+
             var stdOut = new StringBuilder();
             var stdErr = new StringBuilder();
             await Cli.Wrap(info.ADBPath)
-                .WithArguments(["shell", command])
+                .WithArguments(arg)
                 .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOut))
                 .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErr))
                 .ExecuteAsync(token);
 
             var val = T.Parse(stdOut.ToString().Trim(), CultureInfo.InvariantCulture);
             var errors = stdErr.ToString().Trim();
-            Log.Debug("[Exec] adb shell {Command} -> {Result}{Errors}", command, val, errors);
+
+            #if LISTEN_TO_INTEROP && LISTEN_TO_EXECUTIONS
+            Log.Debug("[Exec] adb {Command} -> {Result}{Errors}", arg, val, errors);
+            #endif
             return val;
         }
 
         public async Task<string> Execute(string command, CancellationToken token = default)
         {
+            var arg = $"shell {command}";
+
             var stdOut = new StringBuilder();
             var stdErr = new StringBuilder();
             await Cli.Wrap(info.ADBPath)
-                .WithArguments(["shell", command])
+                .WithArguments(arg)
                 .WithValidation(CommandResultValidation.None)
                 .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOut))
                 .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErr))
@@ -85,10 +109,13 @@ public static class InteropHelper
 
             var val = stdOut.ToString().Trim();
             var errors = stdErr.ToString().Trim();
-            Log.Debug("[Exec] adb shell {Command} -> {Result}{Errors}", command, val, errors);
+
+            #if LISTEN_TO_INTEROP && LISTEN_TO_EXECUTIONS
+            Log.Debug("[Exec] adb {Command} -> {Result}{Errors}", arg, val, errors);
+            #endif
 
             return errors.Length > 0
-                ? throw new Exception(errors)
+                ? throw new NotConnectedException(errors)
                 : val;
         }
     }
