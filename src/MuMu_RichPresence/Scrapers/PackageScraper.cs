ed786e12969779adf455391b12c1a26625536a42
arion <53127594+JustArion@users.noreply.github.com>
2026-03-07 06:36:42 +0100

    Cache Scraper results

--- src/MuMu_RichPresence/Scrapers/PackageScraper.cs
+++ src/MuMu_RichPresence/Scrapers/PackageScraper.cs
@@ -1,10 +1,13 @@
-﻿using Dawn.MuMu.RichPresence.Models;
+﻿using System.Collections.Concurrent;
+using Dawn.MuMu.RichPresence.Models;
 using Polly.Retry;
 
 namespace Dawn.MuMu.RichPresence.Scrapers;
 
 public static class PackageScraper
 {
+    private static readonly ConcurrentDictionary<string, StorePackageInfo?> _packageInfoCache = new();
+
     private static readonly IWebStoreScraper[] _scrapers =
     [
         new PlayStoreScraper(),
@@ -20,16 +23,24 @@ public static class PackageScraper
         return string.Empty;
     }
 
+    // If all the scrapers fail once, we don't keep trying for future requests
     public static async ValueTask<StorePackageInfo?> TryGetPackageInfo(MuMuSessionLifetime session, AsyncRetryPolicy<StorePackageInfo?>? retryPolicy = null)
     {
+        if (_packageInfoCache.TryGetValue(session.PackageName, out var info))
+            return info;
+
         foreach (var scraper in _scrapers)
         {
             var value = await scraper.TryGetPackageInfo(session, retryPolicy);
 
-            if (value != null)
-                return value;
+            if (value == null)
+                continue;
+
+            _packageInfoCache.TryAdd(session.PackageName, value);
+            return value;
         }
 
+        _packageInfoCache.TryAdd(session.PackageName, null);
         return null;
     }
 }
