3b8ee881d4261935d416931d6be6796b4d95aeb4
arion <53127594+JustArion@users.noreply.github.com>
2026-09-10 09:50:54 +0200

    ReSharper recommendation

--- build/Extensions/GitRepositoryEx.cs
+++ build/Extensions/GitRepositoryEx.cs
@@ -7,12 +7,13 @@ using Nuke.Common.Git;
 namespace Extensions;
 
 [SuppressMessage("ReSharper", "InvokeAsExtensionMember")]
+[SuppressMessage("ReSharper", "InvokeAsExtensionMemberFromSameClass")]
 static class GitRepositoryEx
 {
     extension(GitRepository)
     {
         // Reversed since the first tag should be the latest
-        public static ICollection<string> GetTags() => Git("tag", logOutput: false).Select(x => x.Text).Reverse().ToArray();
+        public static ICollection<string> GetTags() => [.. Git("tag", logOutput: false).Select(x => x.Text).Reverse()];
         
         public static string GetTag(Func<string, bool> predicate) => GetTags().FirstOrDefault(predicate);
     }
